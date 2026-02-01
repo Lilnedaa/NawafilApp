b4846c350abc3ad6bcfb468726a9f907683ed263
Nedaa <nedaatlou@gmail.com>
2026-02-01 11:15:55 +0300

    Initial Commit

---
+++ NawafilApp/NawafilAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NawafilAppApp.swift
+//  NawafilApp
+//
+//  Created by Nedaa on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct NawafilAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
