d3ff730c3d9f3a25b8d6951bb6aa81a2947a6daa
Eatzaz <alhafizatzaz111@gmail.com>
2026-02-05 10:00:01 +0300

    ContentView

--- NawafilApp/ContentView.swift
+++ NawafilApp/ContentView.swift
@@ -1,25 +1,74 @@
 //
-//  ContentView.swift
+//  Untitled.swift
 //  NawafilApp
 //
-//  Created by Nedaa on 01/02/2026.
+//  Created by Eatzaz Hafiz on 03/02/2026.
 //
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("صلاة الضحى")
+                    .foregroundColor(.titlecolor)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+                ZStack{
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color.cards, Color.cards.opacity(0.6)
+                                ], startPoint: .topLeading, endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 219, height: 390)
+                        .cornerRadius(24)
+                    VStack {
+                        Text("حكمها")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                        
+                            Text("المساعدة والمساعدات")
+                        Spacer()
+
+
+                    }
+                    .frame(width: 214, height: 390)
+                    
+                    }
+
+                Spacer()
+                    
+            }
+            .navigationBarTitleDisplayMode( .inline )
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "chevron.left")
+                        }
+                        Button{
+                            
+                        } label: {
+                            Image(systemName: "bell.fill")
+                        }
+                    }
+                }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
-//jjjjjjjjjjj
+
