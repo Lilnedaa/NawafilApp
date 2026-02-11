6c02fe07b50e7a46ce14b26efd02b88bf43452f4
shaikha <shaikhavv@gmail.com>
2026-02-11 11:26:07 +0300

    tracker

--- NawafilApp/NTabview.swift
+++ NawafilApp/NTabview.swift
@@ -18,7 +18,7 @@ struct NTabview: View {
        TabView(selection: $selectedTab) {
 
             
-           ContentView(topic: .dhuha)
+           Tracker()
                 .tabItem{
                  Image("Tracker")
                                    .renderingMode(.template)
